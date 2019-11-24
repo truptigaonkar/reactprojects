47a43e48dc0bc8e3c0c4ca3765ecec9babea03b9
truptigaonkar <trups11.trupti@gmail.com>
2019-11-24 11:00:52 +0100

    errorMessage: false/true added

--- src/components/Githubusers.js
+++ src/components/Githubusers.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 import Helmet from 'react-helmet'
 
 class githubusers extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {users:'', errorMessage:''}
+        this.state = { users: '', errorMessage: false }
     }
     // state = {
     //     users: "",
@@ -13,44 +13,53 @@ class githubusers extends Component {
 
     // Form button function
     // getUser(e) {
-        getUser = e => {
+    getUser = e => {
         e.preventDefault();
         const login = e.target.elements.login.value;
+
         axios
             .get(
                 `https://api.github.com/users/${login}`
             )
             .then(response => {
                 console.log(response.data);
-                this.setState({
-                    users: response.data,
-                    login: response.data.login, //showing login on webpage
-                    name:response.data.name //showing name on webpage
-                });
+                if (login) {
+                    this.setState({
+                        //users: response.data,
+                        login: response.data.login, //showing login on webpage
+                        name: response.data.name, //showing name on webpage,
+                        errorMessage: false,
+                    });
+                } else {
+                    this.setState({
+                        login: '',
+                        errorMessage: true,
+                    });
+                }
             })
             .catch(error => {
                 console.log("Error is:", error);
-                  this.setState({
+                this.setState({
                     errorMessage:
-                      "Username does not exist. Please enter valid username"
-                  });
-              });
+                        "Username does not exist. Please enter valid username"
+                });
+            });
     };
 
-      //Disappear error message after 2000sec.
-  componentDidUpdate() {
-    setTimeout(() => this.setState({ errorMessage: "" }), 2000);
-  }
+    // //Disappear error message after 2000sec.
+    // componentDidUpdate() {
+    //     setTimeout(() => this.setState({ errorMessage: "" }), 2000);
+    // }
 
     render() {
         return (
             <>
                 <Helmet><title>Githubusers</title></Helmet>
-                <h1>Github users</h1>
+                <h1>Github Cards</h1>
                 {/* <form onSubmit={this.getUser.bind(this)}> */}
                 <form onSubmit={this.getUser}>
-                    <input type="text" name="login" placeholder="Enter User..." />
-                    <button>Get User</button>
+                    <input type="text" name="login" placeholder="GitHub Username" />
+                    <button>Add Card</button>
                     <p>{this.state.errorMessage}</p>
                     <p>{this.state.login}</p> {/* showing login on webpage */}
                     <p>{this.state.name}</p> {/* showing name on webpage  */}
