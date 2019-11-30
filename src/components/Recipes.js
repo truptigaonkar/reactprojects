a559cbaa715b16fab3bb01cca9e0afc197b212ff
truptigaonkar <trups11.trupti@gmail.com>
2019-11-30 13:56:41 +0100

    merge conflict solved

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -12,7 +12,6 @@ export default class Recipes extends Component {
   getRecipe = e => {
     e.preventDefault();
     const ingredient = e.target.elements.ingredient.value;
-<<<<<<< HEAD
     if (ingredient) {
       axios
         .get(
@@ -28,20 +27,7 @@ export default class Recipes extends Component {
         });
     }
     e.target.reset(); //making input empty
-=======
-    if(ingredient){
-    axios
-      .get(`https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=${ingredient}`) //https://cors-anywhere.herokuapp.com/ is used to remove error Cross-Origin Read Blocking (CORB)
-      .then(response => {
-        console.log(response.data.results);
-        this.setState({
-          recipes: response.data.results,
-          ingredient: response.data.ingredient,
-          title: response.data.title
-        });
-      });
-    }
->>>>>>> 82265061bfa92f1c31956eb2adbd18e37b7754bf
+    
   };
   render() {
     return (
@@ -49,7 +35,6 @@ export default class Recipes extends Component {
       <Helmet><title>Recipe</title></Helmet>
         <h3>Recipe CARD</h3>
         <form onSubmit={this.getRecipe}>
-<<<<<<< HEAD
         <TextField id="standard-basic" name="ingredient" label="Ingredient" placeholder="e.g.fish" />
         </form>
         <div style={{ display: "flex", justifyContent:'space-center', flexWrap: 'wrap' }}>
@@ -61,18 +46,6 @@ export default class Recipes extends Component {
             </div>
           ))}
         </div>
-=======
-          <input type="text" name="ingredient" placeholder="e.g. fish" />
-          <button>GET</button>
-        </form>
-        {this.state.recipes.map(recipe => (
-          <ul key={recipe.id}>
-            <li><a href={`${recipe.href}`} target="_blank"><img src={recipe.thumbnail} alt="" /></a></li>
-            <li><a href={`${recipe.href}`} target="_blank">{recipe.title}</a></li>
-            <li>{recipe.ingredients}</li>
-          </ul>
-        ))}
->>>>>>> 82265061bfa92f1c31956eb2adbd18e37b7754bf
       </>
     );
   }
