4887a2de91bfe538afc510e4cabcdf11a44452d7
truptigaonkar <trups11.trupti@gmail.com>
2019-12-17 22:06:30 +0100

    Updates

---
+++ precache-manifest.7cde5c83cca413d331d7819d315db3b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a596d7241d672065ed84d421c895cf39",
+    "url": "/reactprojects/index.html"
+  },
+  {
+    "revision": "9dc363a67b68f52d8b6c",
+    "url": "/reactprojects/static/css/main.d5d684c7.chunk.css"
+  },
+  {
+    "revision": "75151e0abecce931b64a",
+    "url": "/reactprojects/static/js/2.ea72278a.chunk.js"
+  },
+  {
+    "revision": "9dc363a67b68f52d8b6c",
+    "url": "/reactprojects/static/js/main.c4aeb657.chunk.js"
+  },
+  {
+    "revision": "fef4cfa483c43b22f302",
+    "url": "/reactprojects/static/js/runtime-main.3b5bc720.js"
+  }
+]);
