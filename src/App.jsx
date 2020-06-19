91ce052f4290eb9aa9cb678ce8a9a5aa1e05f0ad
truptigaonkar <trups11.trupti@gmail.com>
2020-06-19 13:38:32 +0200

    Navlink added instead of react router Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
 import Media from './components/media/Mediasearch';
 import Weather from './components/weather/Weathersearch';
 import Recipes from './components/recipes/Recipesearch';
@@ -14,18 +16,20 @@ function App() {
         <header className="header">
           <h1 className="logo"><a href="/">API CARDS</a></h1>
           <ul className="main-nav">
-            <Link to="/">GitHub-User</Link>
-            <Link to="/recipes">Recipe</Link>
-            <Link to="/weather">Weather</Link>
-            <Link to="/media">Media</Link>
-            <Link to="/prisjaktproduct">Prisjakt-Product</Link>
+            <NavLink exact activeClassName="active-class" to="/">GitHub-User</NavLink>
+            <NavLink exact activeClassName="active-class" to="/recipes">Recipe</NavLink>
+            <NavLink exact activeClassName="active-class" to="/weather">Weather</NavLink>
+            <NavLink exact activeClassName="active-class" to="/media">Media</NavLink>
+            <NavLink exact activeClassName="active-class" to="/prisjaktproduct">Prisjakt-Product</NavLink>
           </ul>
         </header>
-        <Route exact path="/" component={Githubusers} />
-        <Route exact path="/recipes" component={Recipes} />
-        <Route exact path="/weather" component={Weather} />
-        <Route exact path="/media" component={Media} />
-        <Route exact path="/prisjaktproduct" component={Prisjaktproduct} />
+        <Switch>
+          <Route exact path="/" component={Githubusers} />
+          <Route path="/recipes" component={Recipes} />
+          <Route path="/weather" component={Weather} />
+          <Route path="/media" component={Media} />
+          <Route path="/prisjaktproduct" component={Prisjaktproduct} />
+        </Switch>
       </Router>
     </div>
   );
